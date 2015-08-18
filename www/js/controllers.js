52ea31809242be662d385dd8b31ead3c7a9483b4
andyafter <andyafter@gmail.com>
2015-08-18 22:13:04 +0800

    added style modification

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -105,10 +105,85 @@ angular.module('starter.controllers', [])
 			title: 'NUS'
 			});
 
+			// for searchBox change
 			google.maps.event.addListener(marker, 'click', function() {
 			infowindow.open(map,marker);
 			});
 
+			var input = document.getElementById('pac-input');
+			var searchBox = new google.maps.places.SearchBox(input);
+			map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+			map.addListener('bounds_changed', function() {
+				searchBox.setBounds(map.getBounds());
+			});
+
+			searchBox.addListener('places_changed', function() {
+				var places = searchBox.getPlaces();
+
+				if (places.length == 0) {
+					return;
+				}
+
+				// Clear out the old markers.
+				markers.forEach(function (marker) {
+					marker.setMap(null);
+				});
+				markers = [];
+
+				// For each place, get the icon, name and location.
+				var bounds = new google.maps.LatLngBounds();
+				places.forEach(function (place) {
+					var icon = {
+						url: place.icon,
+						size: new google.maps.Size(71, 71),
+						origin: new google.maps.Point(0, 0),
+						anchor: new google.maps.Point(17, 34),
+						scaledSize: new google.maps.Size(25, 25)
+					};
+
+					// Create a marker for each place.
+					markers.push(new google.maps.Marker({
+						map: map,
+						icon: icon,
+						title: place.name,
+						position: place.geometry.location
+					}));
+
+					if (place.geometry.viewport) {
+						// Only geocodes have viewport.
+						bounds.union(place.geometry.viewport);
+					} else {
+						bounds.extend(place.geometry.location);
+					}
+				});
+				map.fitBounds(bounds);
+			});
+			///// end of search Box
+
+			/// styles
+			var styles = [
+				{
+					stylers: [
+						{ hue: "#00ffe6" },
+						{ saturation: -20 }
+					]
+				},{
+					featureType: "road",
+					elementType: "geometry",
+					stylers: [
+						{ lightness: 100 },
+						{ visibility: "simplified" }
+					]
+				},{
+					featureType: "road",
+					elementType: "labels",
+					stylers: [
+						{ visibility: "off" }
+					]
+				}
+			];
+			map.set('styles', styles);
+			/// end of styles
 
 			$scope.map = map;
 
@@ -162,10 +237,7 @@ angular.module('starter.controllers', [])
     //google.maps.event.addDomListener(window, 'load', $scope.initialize);
 
   
-})
+});
 
-.controller('SearchMap', function($scope,$http,$ionicLoading, $compile){
-		console.log("only for search map");
-		
-	});
+
 
