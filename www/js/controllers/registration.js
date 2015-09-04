bd7abdbdc923c56c20ed25cbff847fdb6d31e671
andyafter <andyafter@gmail.com>
2015-09-04 14:14:57 +0800

    finished registration

--- www/js/controllers/registration.js
+++ www/js/controllers/registration.js
@@ -13,7 +13,51 @@ var paramlist = ["id","name","aviva_code","zone",
 var paramlist = ['name','ic_num','queue_num','phone_num']
 angular.module('starter.registration', [])
 /// for add tab
-.controller('AddCtrl', function($scope,$http) {
+.controller('AddCtrl', function($scope, $http) {
     $scope.name="andyafter";
     $scope.paramlist = paramlist;
+    $scope.initadd = function () {
+
+    }    
+
+    $scope.register = function(formData){
+        console.log(formData.name);
+        $http.post(urlHead+'registration', formData).success(function(data) {
+            if(data) {
+                //alert(data);
+
+                if('error' in data){
+                    alert(data.error);
+
+                }
+                else{
+                    data = data.success;
+                    var card = document.getElementById("patientcard");
+                    card.style.display="block";
+                    var pname = document.getElementById('patientname');
+                    var pic = document.getElementById('icnum');
+                    var pphone= document.getElementById('phonenum');
+                    var pdoctor = document.getElementById('doctor');
+                    pname.innerHTML ="Name: " + data.name;
+                    pic.innerHTML = "IC Number: " + data.ic_num;
+                    if('phone_num' in data){
+                        pphone.innerHTML = "Phone Number:" + data.phone_num;
+                    }
+
+                    pdoctor.innerHTML = "Doctor: " + data.doctor;
+
+                }
+
+
+            } else {
+                deferred.reject(data);
+            }
+        }).error(function(error) {
+            deferred.reject(error);
+        });
+
+    }
+
+
+
 });
